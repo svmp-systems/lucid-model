c15110b62f9a1f642eae4ef528564178b45bae2a
prnvh <prnvvh@gmail.com>
2026-06-02 20:17:24 +0530

    fixed training generator issues

---
+++ train/tests/generator/test_grid_recolor.py
@@ -0,0 +1,12 @@
+from lucid.training.generator.engine import AmbiguityKnob, check_episode, rng_for_seed
+from lucid.training.generator.recipes import grid_recolor
+
+
+def test_grid_recolor_full_gold():
+    episode = grid_recolor.make(rng_for_seed(2), AmbiguityKnob(0.8))
+    gold = episode.gold
+    assert gold.spans
+    assert gold.frame_targets
+    assert gold.scope_assignments
+    assert gold.basin_families
+    assert not check_episode(episode)
